082a46bb1a8e4ca6ee30363dca97869e71e47ffc
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2024-07-29 14:12:56 +1000

    initial

---
+++ src/Components/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import SupportWorker from "../Assets/support-worker-01.png";
+import SolutionStep from "./SolutionStep";
+import "../Styles/About.css";
+
+function About() {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-image-content">
+        <img src={SupportWorker} alt="Support Worker" className="about-image1" />
+      </div>
+
+      <div className="about-text-content">
+        <h3 className="about-title">
+          <span>About Us</span>
+        </h3>
+        <p className="about-description">
+          Welcome to Christine Walton Independant Support Services, your trusted partner for accessible and
+          personalized support. With years of industry experience, we offer online consultations
+          and specialized services, prioritizing your well-being. Join us on
+          this journey towards a healthier you.
+        </p>
+
+        <h4 className="about-text-title">Your Solutions</h4>
+        <SolutionStep
+          title="Choose Your Support"
+          description="Find the perfect support worker or coordinator tailored to your unique needs. Our experienced professionals are dedicated to providing compassionate and expert assistance, prioritizing your well-being."
+        />
+
+        <SolutionStep
+          title="Schedule with Ease"
+          description="Select the date and time that suits you best, and enjoy the convenience of personalized care at your doorstep. Our dedicated team ensures seamless and reliable support coordination."
+        />
+
+        <SolutionStep
+          title="Achieve Your Goals"
+          description="Receive expert guidance and personalized care plans designed to help you achieve your goals. Whether it's daily support or navigating NDIS plans, we are here to ensure your success and independence."
+        />
+
+      </div>
+    </div>
+  );
+}
+
+export default About;
