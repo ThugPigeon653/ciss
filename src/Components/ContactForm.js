6ade55b3355faa9067c480a5d82b0aaaee9c4be1
TrustedPathways <chriswalt96@gmail.com>
2025-05-06 18:52:58 +1000

    remove unused link

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../Styles/AppointmentForm.css";
