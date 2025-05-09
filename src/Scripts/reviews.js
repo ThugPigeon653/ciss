fbc93f29e0957ec64ae14582a77a14700d60e2f6
TrustedPathways <chriswalt96@gmail.com>
2025-05-10 09:53:14 +1000

    Review Changes
    Changes to the reviews

--- src/Scripts/reviews.js
+++ src/Scripts/reviews.js
@@ -1,52 +1,37 @@
 export const customerReviews = [
   {
-    "name": "Emily Davis",
-    "location": "QLD, Australia",
-    "message": "The support services have made a significant difference in my life. The personalized care and dedicated assistance helped me navigate the NDIS with ease."
+    "name": "Angelika C",
+    "location": "Craignish, QLD",
+    "message": "Trusted Pathways are kind, compassionate, intuitive, caring and reliable. They have made it easier for me to accept that I needed help and that I needed a support worker. The help has made a huge difference to how I can manage my disability. I can't recommend Trusted Pathways enough and know that anyone they support will be in good hands."
   },
   {
-    "name": "Matthew Harris",
+    "name": "Matthew H",
     "location": "QLD, Australia",
-    "message": "I found the perfect support coordinator for my needs. The personalized care plans have been invaluable. Thank you for prioritizing my well-being!"
+    "message": "I found the perfect support coordinator for my needs. The personalized care plans have been invaluable. Thank you for prioritising my well-being!"
   },
   {
-    "name": "Olivia Thompson",
+    "name": "Olivia T",
     "location": "QLD, Australia",
     "message": "Booking appointments was a breeze, and the service exceeded my expectations. The support worker truly listened and provided effective solutions. 5 stars!"
   },
   {
-    "name": "James Wilson",
+    "name": "James W",
     "location": "QLD, Australia",
     "message": "The support services are a game-changer! The emergency assistance provided was quick and efficient, making a critical situation manageable."
   },
   {
-    "name": "Charlotte Walker",
+    "name": "Daniel M",
     "location": "QLD, Australia",
-    "message": "The team made the entire process so much easier. The support worker's dedication and professionalism were exceptional. Highly recommended!"
+    "message": "The assistance I received was outstanding. The support coordinator's expertise and personalised approach made all the difference. Thank you!"
   },
   {
-    "name": "Daniel Martin",
+    "name": "Liam J",
     "location": "QLD, Australia",
-    "message": "The assistance I received was outstanding. The support coordinator's expertise and personalized approach made all the difference. Thank you!"
+    "message": "I highly recommend these support services. The convenience and quality of care have been excellent."
   },
   {
-    "name": "Sophie Anderson",
+    "name": "Ava W",
     "location": "QLD, Australia",
-    "message": "I've been using their services for a while now, and each time, the experience has been wonderful. The support workers are compassionate and reliable."
+    "message": "The personalised care and attention to detail were fantastic. Trusted Pathways made managing my NDIS plan straightforward and stress-free."
   },
-  {
-    "name": "Liam Johnson",
-    "location": "QLD, Australia",
-    "message": "I highly recommend these support services. The convenience and quality of care have been excellent. Great job, team!"
-  },
-  {
-    "name": "Ava White",
-    "location": "QLD, Australia",
-    "message": "The personalized care and attention to detail were fantastic. The team made managing my NDIS plan straightforward and stress-free."
-  },
-  {
-    "name": "Noah Brown",
-    "location": "QLD, Australia",
-    "message": "As a busy professional, the support services have been a lifesaver. Quick and efficient assistance without compromising on quality. Thank you!"
-  }
 ];
